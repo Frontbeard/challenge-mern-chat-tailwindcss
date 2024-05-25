63cd1d2cacfe7ec631738ea9a0dbf26c2f411497
Frontbeard <lufbenitez@gmail.com>
2024-05-25 20:53:22 -0300

    cambios en client

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,7 +3,7 @@ import io from "socket.io-client";
 import { useState, useEffect, useRef } from "react";
 import axios from "axios";
 
-const socket = io("http://localhost:4000");
+const socket = io("https://challenge-react-chat-server.vercel.app/");
 
 function App() {
   const [nickname, setNickname] = useState("");
@@ -16,7 +16,7 @@ function App() {
   const [typingUser, setTypingUser] = useState("");
   const [isCurrentUserTyping, setIsCurrentUserTyping] = useState(false);
   const chatRef = useRef(null);
-  const url = "http://localhost:4000/api/";
+  const url = "https://challenge-react-chat-server.vercel.app/api/";
 
   useEffect(() => {
     const receivedMessage = (message) => {
